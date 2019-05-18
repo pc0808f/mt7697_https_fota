f271944031a7c4682d44cf6a04d8c52a20593ccf
pc0808f <pc0808f@gmail.com>
2019-05-19 01:45:08 +0800

    3 partition for flash.ld and flash_map.h

--- inc/flash_map.h
+++ inc/flash_map.h
@@ -39,15 +39,19 @@
 #define RESERVED_LENGTH         0x8000        /*  32KB */
 #define N9_RAMCODE_LENGTH       0x69000       /* 420KB */
 #define CM4_CODE_LENGTH         0x1ED000      /* 1972KB */
-#define FOTA_LENGTH             0x18A000      /* 1576KB */
+#define ORI_LENGTH              0xC1000       /* 772KB */
+#define FOTA_LENGTH             0xC1000       /* 772KB */
+#define EXTBL_LENGTH            0x8000        /*  32KB */
 #define NVDM_LENGTH             0x10000       /*  64KB */
 
 #define LOADER_BASE             0x0
 #define RESERVED_BASE           (LOADER_BASE     + LOADER_LENGTH)
 #define N9_RAMCODE_BASE         (RESERVED_BASE   + RESERVED_LENGTH)
 #define CM4_CODE_BASE           (N9_RAMCODE_BASE + N9_RAMCODE_LENGTH)
-#define FOTA_BASE               (CM4_CODE_BASE   + CM4_CODE_LENGTH)
-#define NVDM_BASE               (FOTA_BASE       + FOTA_LENGTH)
+#define ORI_BASE                (CM4_CODE_BASE   + CM4_CODE_LENGTH)
+#define FOTA_BASE               (ORI_BASE        + ORI_LENGTH)
+#define EXTBL_BASE              (FOTA_BASE       + FOTA_LENGTH)
+#define NVDM_BASE               (EXTBL_BASE      + EXTBL_LENGTH)
 
 #define FLASH_BASE		        0x10000000
 
