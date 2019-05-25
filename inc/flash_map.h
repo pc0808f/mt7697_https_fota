8592c2763de5dd0e51b51a0d7e5634387c8a35b9
pc0808f <pc0808f@gmail.com>
2019-05-26 07:07:54 +0800

    change ext bootloader image location

--- inc/flash_map.h
+++ inc/flash_map.h
@@ -39,19 +39,19 @@
 #define RESERVED_LENGTH         0x8000        /*  32KB */
 #define N9_RAMCODE_LENGTH       0x69000       /* 420KB */
 #define CM4_CODE_LENGTH         0x1ED000      /* 1972KB */
-#define ORI_LENGTH              0xC1000       /* 772KB */
-#define FOTA_LENGTH             0xC1000       /* 772KB */
-#define EXTBL_LENGTH            0x8000        /*  32KB */
+#define EXTBL_IMAGE_LENGTH      0x8000        /*  32KB */
+#define ORI_IMAGE_LENGTH        0xC1000       /* 772KB */
+#define FOTA_IMAGE_LENGTH       0xC1000       /* 772KB */
 #define NVDM_LENGTH             0x10000       /*  64KB */
 
 #define LOADER_BASE             0x0
 #define RESERVED_BASE           (LOADER_BASE     + LOADER_LENGTH)
 #define N9_RAMCODE_BASE         (RESERVED_BASE   + RESERVED_LENGTH)
 #define CM4_CODE_BASE           (N9_RAMCODE_BASE + N9_RAMCODE_LENGTH)
-#define ORI_BASE                (CM4_CODE_BASE   + CM4_CODE_LENGTH)
-#define FOTA_BASE               (ORI_BASE        + ORI_LENGTH)
-#define EXTBL_BASE              (FOTA_BASE       + FOTA_LENGTH)
-#define NVDM_BASE               (EXTBL_BASE      + EXTBL_LENGTH)
+#define EXTBL_IMAGE_BASE        (CM4_CODE_BASE   + CM4_CODE_LENGTH)
+#define ORI_IMAGE_BASE          (EXTBL_IMAGE_BASE+ EXTBL_IMAGE_LENGTH)
+#define FOTA_IMAGE_BASE         (ORI_IMAGE_BASE  + ORI_IMAGE_LENGTH)
+#define NVDM_BASE               (FOTA_IMAGE_BASE + FOTA_IMAGE_LENGTH)
 
 #define FLASH_BASE		        0x10000000
 
